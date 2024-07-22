8fe7f019e7a6ddbe16a884c6e3fd9f0a01250903
qaq <yuhangzeng13@gmail.com>
2024-07-23 04:59:40 +0800

    chore: pnpm 1

---
+++ src/components/AppSwiper.vue
@@ -0,0 +1,27 @@
+<template>
+  <uv-swiper
+    :list="list"
+    keyName="image"
+    showTitle
+    :title-style="{ color: 'red', textAlign: 'center' }"
+    :autoplay="false"
+    circular
+  ></uv-swiper>
+</template>
+
+<script>
+export default {
+  name: 'AppSwiper',
+  props: {
+    list: {
+      type: Array,
+      required: true,
+      default: () => [],
+    },
+  },
+}
+</script>
+
+<style scoped>
+/* 你可以在这里添加组件的样式 */
+</style>
